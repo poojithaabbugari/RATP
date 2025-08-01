b2a6af4341a596fd738d1a5b7cad418ac6ae2f12
ABBUGARI POOJITHA <poojithaabbugari041@gmail.com>
2025-08-01 22:09:44 +0530

    Add files via upload

---
+++ rq.py
@@ -0,0 +1,22 @@
+# Program for Right-Angled Triangle Pattern (Left-Aligned)
+
+for i in range(5):  #i indicates the number of rows and they are 4
+    for j in range(i+1):  #j indicates the columns,the stars are printed based on i+1
+        print("*",end=" ")  #After prints the * and end is used to ends in the same line
+    print()  #it is used to print the * in the nextline
+
+
+
+
+
+
+
+    """
+    *
+    * *
+    * * *
+    * * * *
+    * * * * *
+    
+    
+    """
